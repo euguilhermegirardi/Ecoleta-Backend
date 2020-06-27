bb27d2686e1eb644e5a042b348c8003883ff77ee
Guilherme Girardi <girardi.gui@icloud.com>
2020-06-27 15:06:42 +0100

    Edit migrations

--- src/database/migrations/00_create_points.ts
+++ src/database/migrations/00_create_points.ts
@@ -6,14 +6,14 @@ export async function up(knex: Knex) {
   // Create the table
   return knex.schema.createTable("points", (table) => {
     table.increments("id").primary();
-    table.string("image").notNullable;
-    table.string("name").notNullable;
-    table.string("email").notNullable;
-    table.string("whatsapp").notNullable;
-    table.decimal("latitude").notNullable;
-    table.decimal("longitude").notNullable;
-    table.string("city").notNullable;
-    table.string("uf", 2).notNullable;
+    table.string("image").notNullable();
+    table.string("name").notNullable();
+    table.string("email").notNullable();
+    table.string("whatsapp").notNullable();
+    table.decimal("latitude").notNullable();
+    table.decimal("longitude").notNullable();
+    table.string("city").notNullable();
+    table.string("uf", 2).notNullable();
   });
 }
 
--- src/database/migrations/01_create_items.ts
+++ src/database/migrations/01_create_items.ts
@@ -6,8 +6,8 @@ export async function up(knex: Knex) {
   // Create the table
   return knex.schema.createTable("items", (table) => {
     table.increments("id").primary();
-    table.string("image").notNullable;
-    table.string("title").notNullable;
+    table.string("image").notNullable();
+    table.string("title").notNullable();
   });
 }
 
